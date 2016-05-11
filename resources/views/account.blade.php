4e90dadea1e266301d99f276fce4af4056d294de
Margarita Rafaeli <mar3500@g.harvard.edu>
2016-05-11 13:28:28 -0400

    fixing up account form

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -15,38 +15,52 @@
         <div class="panel-body">
             <form method='POST' action='/account' enctype="multipart/form-data" >
                 {{ csrf_field() }}
-                <div class='form-group'>
-                    <label>Profile Picture:</label><br>
-                @if(Auth::user()->profile_picture !== null)
-                    <img id="photo" width="150" height="150"  class="img-thumbnail img-left" src="{{ Auth::user()->profile_picture }}" alt="{{ Auth::user()->name }}">
-                @else
-                    <img id="photo" width="150" height="150" class="img-thumbnail img-left" src="{{URL::asset('assets/img/default_profile_avatar.jpg')}}" alt="">
-                @endif
-                    <input type="file" name="profile_picture" onchange="document.getElementById('photo').src = window.URL.createObjectURL(this.files[0])">
+                <div class='form-group row'>
+                    <label class="col-sm-2 control-label">Profile Picture:</label><br>
+                    <div class="col-sm-10">
+                    @if(Auth::user()->profile_picture !== null)
+                        <img id="photo" width="150" height="150"  class="img-thumbnail img-left" src="{{ Auth::user()->profile_picture }}" alt="{{ Auth::user()->name }}">
+                    @else
+                        <img id="photo" width="150" height="150" class="img-thumbnail img-left" src="{{URL::asset('assets/img/default_profile_avatar.jpg')}}" alt="">
+                    @endif
+                        <input type="file" name="profile_picture" onchange="document.getElementById('photo').src = window.URL.createObjectURL(this.files[0])">
+                    </div>
                 </div>
-                <div class='form-group'>
-                    <label>Name:</label>
-                    <input type='text' name="name" value="{{ Auth::user()->name }}">
+                <div class='form-group row'>
+                    <label class="col-sm-2 control-label">Name:</label>
+                    <div class="col-sm-10">
+                        <input type='text' name="name" value="{{ Auth::user()->name }}">
+                    </div>
                 </div>
-                <div class='form-group'>
-                    <label>Email:</label>
-                    <span>{{ Auth::user()->email }}</span>
+                <div class='form-group row'>
+                    <label class="col-sm-2 control-label">Email:</label>
+                    <div class="col-sm-10">
+                        <span>{{ Auth::user()->email }}</span>
+                    </div>
                 </div>
-                <div class='form-group'>
-                    <label>Account Type:</label>
-                    <span>{{ Auth::user()->is_parent ? "Parent/Guardian" : "Caregiver" }}</span>
+                <div class='form-group row'>
+                    <label class="col-sm-2 control-label">Account Type:</label>
+                    <div class="col-sm-10">
+                        <span>{{ Auth::user()->is_parent ? "Parent/Guardian" : "Caregiver" }}</span>
+                    </div>
                 </div>
-                <div class='form-group'>
-                    <label>Old Password:</label>
-                    <input type='password' name="old_password">
+                <div class='form-group row'>
+                    <label class="col-sm-2 control-label">Old Password:</label>
+                    <div class="col-sm-10">
+                        <input type='password' name="old_password">
+                    </div>
                 </div>
-                <div class='form-group'>
-                    <label>New Password:</label>
-                    <input type='password' name="new_password">
+                <div class='form-group row'>
+                    <label class="col-sm-2 control-label">New Password:</label>
+                    <div class="col-sm-10">
+                        <input type='password' name="new_password">
+                    </div>
                 </div>
-                <div class='form-group'>
-                    <label>Confirm New Password:</label>
-                    <input type='password' name="confirm_password">
+                <div class='form-group row'>
+                    <label class="col-sm-2 control-label">Confirm New Password:</label>
+                    <div class="col-sm-10">
+                        <input type='password' name="confirm_password">
+                    </div>
                 </div>
                 <button type='submit' class="btn btn-primary">Update</button>
             </form>
